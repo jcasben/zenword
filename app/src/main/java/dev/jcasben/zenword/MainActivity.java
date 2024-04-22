0a4a1b4419138f9fb3428a1f801e7bcac54cae7f
jcasben <jcasben02@gmail.com>
2024-04-22 19:52:53 +0200

    Fix: corrected fatal errors

--- app/src/main/java/dev/jcasben/zenword/MainActivity.java
+++ app/src/main/java/dev/jcasben/zenword/MainActivity.java
@@ -1,14 +1,20 @@
 package dev.jcasben.zenword;
 
 import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     String referenceWord;
+    int[] ids ={R.id.buttonL0, R.id.buttonL1, R.id.buttonL2, R.id.buttonL3,
+            R.id.buttonL4, R.id.buttonL5, R.id.buttonL6};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +23,14 @@ public class MainActivity extends AppCompatActivity {
         // Get reference word
         referenceWord = getReferenceWord();
         // Generate the buttons of the possible solution words.
-        setPossibleSolutionWordsButtons();
+        setPossibleSolutionWordsTextV();
         // Generate buttons of the circle
         setCircleButtonLetters();
-
+        suffle(null);
     }
 
 
-    // onClick buttons function
+    // onClick letter buttons function
     public void setLetter(View view){
         Button but = (Button) view;
         //Get the letter of the button.
@@ -38,26 +44,68 @@ public class MainActivity extends AppCompatActivity {
         //Enable button.
         but.setEnabled(false);
     }
+
     private String getReferenceWord(){
-        return "banc";
+        return "castaña".toUpperCase();
     }
 
-    private void setPossibleSolutionWordsButtons(){
+    private void setPossibleSolutionWordsTextV(){
 
     }
+
     //name of the buttons: l0, l1, l2, l3, l4, l5, l6
     private void setCircleButtonLetters(){
-        int[] ids ={R.id.buttonL0, R.id.buttonL1, R.id.buttonL2, R.id.buttonL3,
-                R.id.buttonL4, R.id.buttonL5, R.id.buttonL6};
         int i;
         for (i = 0; i < referenceWord.length(); i++) {
             Button bLetter = findViewById(ids[i]);
             bLetter.setOnClickListener(e -> setLetter(bLetter));
-            bLetter.setText(referenceWord.charAt(i));
+            bLetter.setText(new char[]{referenceWord.charAt(i)}, 0, 1);
         }
-        for (i = referenceWord.length() - i; i < referenceWord.length(); i++) {
+        for ( ;i < 7; i++) {
             Button button = findViewById(ids[i]);
-            button.setVisibility(View.INVISIBLE);
+            button.setVisibility(View.GONE);
         }
+    }
+
+    // onClick random button function
+    public void suffle (View view) {
+        Random ran = new Random();
+        char[] word = referenceWord.toCharArray();
+        // suffle the word
+        for (int i = 0; i < word.length; i++) {
+            int j = ran.nextInt(word.length);
+            char aux = word[i];
+            word[i] = word[j];
+            word[j] = aux;
+        }
+        // put the suffle word on the buttons
+        for (int i = 0; i < referenceWord.length(); i++) {
+            Button bLetter = findViewById(ids[i]);
+            bLetter.setText(word, i, 1);
+        }
+    }
+
+    // onClick bonus button
+    public void bonus(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("encertades i possibles");
+        builder.setMessage("La llista de trobades: \n");
+
+        // OK button for close the window
+        builder.setPositiveButton("OK", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+    public void clear(View view) {
+        //restart buttons
+        for (int i = 0; i < referenceWord.length(); i++) {
+            Button b = findViewById(ids[i]);
+            b.setEnabled(true);
+        }
+        //clear text view
+        TextView tv = findViewById(R.id.textVWordFormation);
+        tv.setText("");
     }
 }
